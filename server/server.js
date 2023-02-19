ef322af973966ff3bb223a0510da94922bc52c97
JDCPrograms2017 <jchadwick423@gmail.com>
2023-02-19 06:46:37 -0800

    death by backend

--- server/server.js
+++ server/server.js
@@ -1,45 +1,61 @@
+import { getAuth, signInAnonymously } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
+
 const express = require('express');
-const bodyParser = require('body-parser');
-const { Pool, Client } = require('pg');
-
-const app = express();
-const port = process.env.PORT || 3000;
-app.use(express.urlencoded({extended: true})); 
-app.use(express.json());
-
-const connectionString = 'postgresql://joshua:O0nUO5XDUlLJ-YjBn-Mtfw@swamp-manta-5267.6wr.cockroachlabs.cloud:26257/interestfields?sslmode=verify-full'
- 
-const pool = new Pool({
-  connectionString,
-})
+const bodyParser = require('body-parse');
+const expressapp = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
- 
-app.get('/insert', function(req, res) {
-  var sql = {
-    username: req.body.name,
-    email: req.body.email,
-    phone: req.body.phone
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+const firebaseConfig = {
+    apiKey: "AIzaSyCW27AdN_XZINa7yGkEElHhv_Iw7sYVNKs",
+    authDomain: "nodesocial-742c2.firebaseapp.com",
+    projectId: "nodesocial-742c2",
+    storageBucket: "nodesocial-742c2.appspot.com",
+    messagingSenderId: "518737948604",
+    appId: "1:518737948604:web:d8c419976ca331e7c045b9",
+    measurementId: "G-K477B38MK2"
   };
 
-  pool.query('INSERT INTO interestfields SET ?', sql, (err, res) => {
-    console.log(err, res)
-    pool.end()
-  })
-  res.redirect('/')
-})
- 
-const client = new Client({
-  connectionString,
-})
-client.connect()
- 
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  client.end()
-})
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+const auth = getAuth();
+const form = document.querySelector('thisform');
+
+form.addEventListener('submit', async (event) => {
+    console.log("heard!");
+    event.preventDefault();
+    signInAnonymously(auth).then(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                const uid = user.uid;
+                app.post('/insert', (req, res) => {
+                    const data = {
+                        username: req.body.name,
+                        useremail: req.body.email,
+                        userphone: req.body.phone
+                    };
+                
+                    setDoc(doc(db, "interests", `${uid}`), data);
+                // ...
+                });
+
+            } else {
+                console.log("Bye bye user!");
+            }
+        });
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    });
 });
